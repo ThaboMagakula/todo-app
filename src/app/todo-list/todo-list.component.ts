85aadc01b5117c489ce95a55b97091e31c4906af
Thabo Magakula <thabo.magakula@gmail.com>
2018-08-14 13:59:44 +0200

    Part 2. Adding of all the new components

---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, Input,Output, EventEmitter } from '@angular/core';
+
+import  {Todo} from '../todo';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css']
+})
+export class TodoListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  @Input()
+  todos: Todo[];
+
+  @Output()
+  remove: EventEmitter<Todo> = new EventEmitter();
+
+  @Output()
+  toggleComplete: EventEmitter<Todo> = new EventEmitter();
+
+  onToggleTodoComplete(todo: Todo) {
+    this.toggleComplete.emit(todo);
+  }
+
+  onRemoveTodo(todo: Todo) {
+    this.remove.emit(todo);
+  }
+
+}
